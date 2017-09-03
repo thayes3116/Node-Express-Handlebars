db559652d3ac752b225b8785a62a8e93fa12af5c
thayes3116 <timhayes3116@gmail.com>
2017-09-03 15:50:04 -0400

    created orm.js file. connected connection.js and model.js.  Working on cb functions and mysql queries

---
+++ config/orm.js
@@ -0,0 +1,39 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+
+	selectSpecies: function(cb){
+		
+		connection.query("SELECT `species`.`Species_ID`, `species`.`Common_Names`, `species`.`Park_Name`, `parks`.`State`, `species`.`Conservation_Status` FROM `species` INNER JOIN `parks` ON `species`.PARK_NAME = `parks`.`Park_Name` WHERE `parks`.`State` = 'CA' AND (`species`.`Conservation_Status` = 'Endangered' OR `Conservation_Status` = 'Species of Concern') LIMIT 10;", function(err, results){
+
+	 	 	if (err) throw err;
+
+	 	 	cb(results);
+
+	 	 });
+	},
+
+	addSpecies: function(col, val,  cb){
+
+      connection.query("INSERT INTO `species` (col) VALUES (?)", [val], function(err, result) {
+          
+          if (err) throw err;
+      cb(result);
+      });
+   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+module.exports = orm;
--- models/model.js
+++ models/model.js
@@ -1,16 +1,11 @@
-var connection = require("../config/connection.js");
+var orm = require('../config/orm.js');
 
  var model ={
 	 selectSpecies: function(cb){
-	 	 connection.query("SELECT `species`.`Species_ID`, `species`.`Common_Names`, `species`.`Park_Name`, `parks`.`State`, `species`.`Conservation_Status` FROM `species` INNER JOIN `parks` ON `species`.PARK_NAME = `parks`.`Park_Name` WHERE `parks`.`State` = 'CA' AND (`species`.`Conservation_Status` = 'Endangered' OR `Conservation_Status` = 'Species of Concern') LIMIT 10;", function(err, results){
-	 	 	    if (err) {
-            throw err;
-          } 
-
-	 	 	cb(results);	 	 	
-	 	 });
+	 	 orm.selectSpecies(function(data){
+      cb(data);
+     });
 	 },
-
    addSpecies: function(col, val,  cb){
       connection.query("INSERT INTO `species` (col) VALUES (?)", [val], function(err, result) {
           if (err) throw err;
